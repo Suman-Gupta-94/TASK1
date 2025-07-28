b9ba46954513e209ee070b04bb1a279a9cc43cd8
Suman-Gupta-94 <sumantele94@gmail.com>
2025-07-28 15:06:03 +0530

    Add files via upload

---
+++ ASSIGNMENT 1.py
@@ -0,0 +1,28 @@
+
+#Takes a user's first name and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+#Concatenates the first name and last name into a full name
+full_name = first_name + " " + last_name
+#Print a personalized greeting message using the full name
+print(f"Hello, {full_name}! Welcome to the Python programming world.")
+
+a=5
+b=10
+c=a+b
+print("additon",c)
+
+a=5
+b=10
+c=a-b
+print("Substraction: ",c)
+
+a=5
+b=10
+c=a*b
+print("multiplication :",c)
+
+a=5
+b=10
+c=a/b
+print("division:" ,c)
